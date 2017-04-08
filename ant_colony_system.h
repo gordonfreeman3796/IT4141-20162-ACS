f53a2ddc6d08c76ac8b1373d0fa145593d5abad0
gordonfreeman3796 <gordonnguyen3796@gmail.com>
2017-04-08 11:07:06 +0700

    Debugging

--- ant_colony_system.h
+++ ant_colony_system.h
@@ -3,33 +3,33 @@
 
 #define MAX_IT 100
 #define NUM_ANTS 10
+#define CITY_SIZE 52
 #define DECAY 0.1
 #define C_HEUR 2.5
 #define C_LOCAL_PHERO 0.1
 #define C_GREED 0.9
-#define CITY_SIZE 52
 
 typedef struct
 {
-    int vector[52], cost;
+    int vector[CITY_SIZE], cost;
 } Solution;
 
 typedef struct
 {
     int city, distance;
-    float history, heuristice, prob;
+    float history, heuristic, prob;
 } Probability;
 
-int search(int**, int, int, float, float, float, float);
+void search(int[][2], int, int, float, float, float, float);
 int euc_2d(int*, int*);
-int cost(int*, int**);
-void random_permutation(int*, int);
-void initialise_pheromone_matrix(int**, int, float);
-void calculate_choices(Probability*, int**, int, int*, int**, float, float);
+int cost(int*, int[][2]);
+void random_permutation(int*);
+void initialise_pheromone_matrix(float[][CITY_SIZE], float);
+int calculate_choices(int, Probability*, int[][2], int, int*, float[][CITY_SIZE], float, float);
 int prob_select(Probability*, int);
-void greedy_select(Probability*, int);
-void stepwise_const(int*, int**, int**, float, float);
-void global_update_pheromone(float**, Solution, float);
-void local_update_pheromone(float**, Solution, float, float);
+int greedy_select(Probability*, int);
+void stepwise_const(int*, int[][2], float[][CITY_SIZE], float, float);
+void global_update_pheromone(float[][CITY_SIZE], Solution, float);
+void local_update_pheromone(float[][CITY_SIZE], Solution, float, float);
 
 #endif
